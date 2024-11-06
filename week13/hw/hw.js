502038594c23d83a3b8f79fefe29e178473c92fd
Natalia1008 <n.bubnova93@gmail.com>
2024-11-06 23:01:32 +0300

    2 option

--- week13/hw/hw.js
+++ week13/hw/hw.js
@@ -1,37 +1,4 @@
+function calculateDays () {
 
-
-function getDay()
-{
-
-const birthdayString = document.getElementById('bday').value;
-const birthday= new Date(birthdayString);
-const today = new Date();
-const currentYear = today.getFullYear();
-const birthdayInCurrentYear = birthday.setFullYear(currentYear);
-let days = Math.floor((today - birthdayInCurrentYear) / (1000 * 60 * 60 *24));
-
-if (days < 0) {
-    days = days + 365;
+    
 }
-
-if (days >= 11 && days <=20) textDays = 'дней';
-
-
-let textDays = 'дней';
-
-document.getElementById('result').innerHTML = `До дня рождения осталось: ${days} ${textDays}`;
-
-const daysEnding = getDaysEnding(days);
-
-if (daysEnding === 1) textDays = 'день';
-else if (daysEnding <= 4 && daysEnding >= 2) textDays = 'дня';
-}
-
-function getDaysEnding(number) {
-    const lastTwoDigits = number % 100;
-    const lastDigit = number % 10;
-    if (number < 10) return number;
-    if (number >= 10 && number < 100) return lastDigit;
-    return lastTwoDigits;
-}
-
